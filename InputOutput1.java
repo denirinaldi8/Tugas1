1b734be87af5804289fb0a842630d23f7c6dd815
denirinaldi8 <denirinaldi13@gmail.com>
2017-03-17 17:27:44 +0700

    Add files via upload

---
+++ InputOutput1.java
@@ -0,0 +1,7 @@
+public class InputOutput1 {
+	public static void main(String[] args) {
+		System.out.print ("Hello World");
+		System.out.println ("Hello World");
+		System.out.print ("Hello World");
+	}
+}
